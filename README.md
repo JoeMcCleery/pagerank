ccd903d390040eb81ec856f546c272c376c910c6
FearIndigo <josephm@fearindigo.com.au>
2022-07-04 13:36:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# pagerank
+ cs50ai Project 2a
+ 
+```
+py pagerank.py corpus0
+py pagerank.py corpus1
+py pagerank.py corpus2
+```
---
+++ pagerank.py
@@ -0,0 +1,117 @@
+import os
+import random
+import re
+import sys
+
+DAMPING = 0.85
+SAMPLES = 10000
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+
+
+def crawl(directory):
+    """
+    Parse a directory of HTML pages and check for links to other pages.
+    Return a dictionary where each key is a page, and values are
+    a list of all other pages in the corpus that are linked to by the page.
+    """
+    pages = dict()
+
+    # Extract all links from HTML files
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
+            pages[filename] = set(links) - {filename}
+
+    # Only include links to other pages in the corpus
+    for filename in pages:
+        pages[filename] = set(
+            link for link in pages[filename]
+            if link in pages
+        )
+
+    return pages
+
+
+def transition_model(corpus, page, damping_factor):
+    """
+    Return a probability distribution over which page to visit next,
+    given a current page.
+
+    With probability `damping_factor`, choose a link at random
+    linked to by `page`. With probability `1 - damping_factor`, choose
+    a link at random chosen from all pages in the corpus.
+    """
+    # Initialize probability distribution
+    dist = dict()
+    # Get number of pages in corpus
+    total_pages = len(corpus)
+    # Get number of links in page
+    num_links = len(corpus[page])
+    # If no links on page
+    if num_links == 0:
+        # Get chance to navigate to any page
+        chance = 1.0 / total_pages
+        # Populate probability distribution
+        for p in corpus:
+            dist[p] = chance
+    else:
+        # Get random chance to navigate to any page in the corpus
+        random_chance = (1.0 - damping_factor) / total_pages
+        # Get chance to navigate to one of the links on page
+        link_chance = damping_factor / num_links
+        # Populate probability distribution
+        for p in corpus:
+            # If current page is linked to on page
+            if p in corpus[page]:
+                # Set chance to link_chance add random_chance
+                dist[p] = link_chance + random_chance
+            else:
+                # Set chance to random_chance
+                dist[p] = random_chance
+    return dist
+
+
+
+
+def sample_pagerank(corpus, damping_factor, n):
+    """
+    Return PageRank values for each page by sampling `n` pages
+    according to transition model, starting with a page at random.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    raise NotImplementedError
+
+
+def iterate_pagerank(corpus, damping_factor):
+    """
+    Return PageRank values for each page by iteratively updating
+    PageRank values until convergence.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
